cfea79e0b90c1360020cdfe9dbb9fd150d9fc5ef
EC2 Default User <ec2-user@ip-172-31-92-143.ec2.internal>
2020-05-21 18:12:06 +0000

    updated dockerfile

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,11 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=sampletest19/flaskapp
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push flaskapp
