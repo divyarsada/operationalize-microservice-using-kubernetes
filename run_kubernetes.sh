b272f42e4c453db26f07572c1f441f1bc1d711d8
EC2 Default User <ec2-user@ip-172-31-90-159.ec2.internal>
2020-05-22 05:39:58 +0000

    Update docker,kubectl commands to build image,upload image to docker hub and run cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sampletest19/samplecontainerizedflaskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskappln --image=sampletest19/samplecontainerizedflaskapp:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pods/flaskappln-54cd69c94c-bmvs7 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,11 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=sampletest19/flaskapp
+dockerpath=sampletest19/samplecontainerizedflaskapp
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+#docker tag <existing-image> <hub-user>/<repo-name>[:<tag>]
+docker tag flaskapp sampletest19/samplecontainerizedflaskapp:latest
+#docker commit sampletest19/samplecontainerizedflaskapp:latest
 # Step 3:
 # Push image to a docker repository
-docker push flaskapp
+docker push sampletest19/samplecontainerizedflaskapp:latest
